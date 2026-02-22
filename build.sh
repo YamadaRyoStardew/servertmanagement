426f35061d793308fe90f06e53fba46d98ff4f93
YamadaRyoStardew <herewehavequalityhotpot@gmail.com>
2026-02-22 09:09:19 +0700

    địt mẹ crave

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Build LineageOS for a13ve (Samsung A13 4G)
-
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs
+/opt/crave/resync.sh
 # Clean
 rm -rf hardware/samsung
 rn -rf hardware/mediatek
@@ -13,8 +14,9 @@ git clone https://cuynutt.ddns.net/ncatt/vendor vendor/samsung/a32
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_samsung -b lineage-23.2 hardware/samsung
 
+
 # Build
 echo "-> Build started!"
 source build/envsetup.sh
-croot
-brunch a13ve
+launch lineage_a32-bp4a-userdebug
+mka bacon
