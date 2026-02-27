6c75f37a32dfbb2e6f0b8663a310be39d7c9db17
YamadaRyoStardew <herewehavequalityhotpot@gmail.com>
2026-02-27 13:08:34 +0700

    Update git clone commands in build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ rm -rf hardware/mediatek
 # Clone trees
 git clone https://cuynutt.ddns.net/ncatt/clang prebuilts/clang/host/linux-x86/clang-r383902
 git clone https://cuynutt.ddns.net/ncatt/dt device/samsung/a32
-kernel/samsung/mt6768
+git clone https://cuynutt.ddns.net/ncatt/kernel kernel/samsung/mt6768
 git clone https://cuynutt.ddns.net/ncatt/hw-mtk hardware/mediatek
 git clone https://cuynutt.ddns.net/ncatt/vendor vendor/samsung/a32
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/LineageOS/android_hardware_samsung -b lineage-23.2 hardware/samsung
+  git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/yaap/hardware_samsung -b fifteen hardware/samsung
 
 
 # Build
